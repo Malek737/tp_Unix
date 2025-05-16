f62ad26d66190b5988d487aeed7b7fc233278686
#Guigui <hastag.guigui@outlook.com>
2025-05-16 10:47:28 +0200

    fix line 14

---
+++ exo1_tp_Unix.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+
+rm -f ./fifo
+mkfifo ./fifo
+function interpret () {
+
+    echo "Bienvenue, utilisateur."
+    CONN_TIME=$(date '+%Y-%m-%d %H:%M:%S')
+    echo "Date de connexion : $CONN_TIME"
+
+    while read line; do
+        echo "Debug, reçu == $line ==" >&2
+
+         if [ $line = "exit" ]; then 
+            echo "Date de déconnexion : $DISCONN_TIME"
+            exit
+        fi
+    done
+}
+echo "Démarrage de l'écoute..."
+while true; do
+    echo "En attente de connexion..."
+    nc -l -s localhost -p 12345 < ./fifo | interpret > ./fifo
+    echo "Connexion terminée"
+done
+
+echo "end."
+
+
+
+
+
+
+
+
